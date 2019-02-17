5c4ad1cdc69c9e770c65b387d6c8ce139b2a8bb2
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-02-17 07:51:54 +0100

    Step 1.3.3 - refactor Exporter so that it extends AbstractExporter

--- src/main/java/com/example/warehouse/export/Exporter.java
+++ src/main/java/com/example/warehouse/export/Exporter.java
@@ -8,23 +8,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public final class Exporter {
+public final class Exporter extends AbstractExporter {
 
     private static final String BORDER = "*";
     private static final String LEFT_BORDER = BORDER + " ";
     private static final String RIGHT_BORDER = " " + BORDER;
     private static final String SEPARATOR = " | ";
 
-    private final Report report;
-    private final PrintStream out;
     private final List<Integer> widths;
 
     private final int totalWidth;
 
     public Exporter(Report report, PrintStream out) {
-        this.report = report;
-        this.out = out;
-        this.widths = calcWidths();
+        super(report, out);
+        this.widths = calcWidths(report);
 
         totalWidth = widths.stream().mapToInt(Integer::intValue).sum()
             + LEFT_BORDER.length()
@@ -32,15 +29,7 @@ public final class Exporter {
             + RIGHT_BORDER.length();
     }
 
-    public void export() {
-        printBorder();
-        printStrings(report.getLabels());
-        printBorder();
-        report.getRecords().forEach(this::printStrings);
-        printBorder();
-    }
-
-    private List<Integer> calcWidths() {
+    private List<Integer> calcWidths(Report report) {
         List<Integer> widths = new ArrayList<>();
         report.getLabels().forEach(l -> widths.add(l.length()));
         for (List<String> record : report.getRecords()) {
@@ -55,14 +44,39 @@ public final class Exporter {
         return widths;
     }
 
-    private void printBorder() {
+    @Override
+    protected void beforeLabels(PrintStream out) {
+        printBorder(out);
+    }
+
+    @Override
+    protected void handleLabels(PrintStream out, List<String> labels) {
+        printStrings(out, labels);
+    }
+
+    @Override
+    protected void afterLabels(PrintStream out) {
+        printBorder(out);
+    }
+
+    @Override
+    protected void handleRecord(PrintStream out, List<String> records, boolean first, boolean last) {
+        printStrings(out, records);
+    }
+
+    @Override
+    protected void afterRecords(PrintStream out) {
+        printBorder(out);
+    }
+
+    private void printBorder(PrintStream out) {
         for (int i = 0; i < totalWidth; i++) {
             out.print(BORDER);
         }
         out.println();
     }
 
-    private void printStrings(List<String> strings) {
+    private void printStrings(PrintStream out, List<String> strings) {
         out.print(LEFT_BORDER);
         out.print(IntStream.range(0, strings.size())
             .mapToObj(i -> {
