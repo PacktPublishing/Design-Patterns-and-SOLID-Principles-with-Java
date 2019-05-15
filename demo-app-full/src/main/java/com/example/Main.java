903eee97d1793f3bb4f4357c96ab78c09ff3aa3f
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-05-15 17:02:06 +0200

    Multi-module Maven project refactor (step 2)

--- demo-app-full/src/main/java/com/example/Main.java
+++ demo-app-full/src/main/java/com/example/Main.java
@@ -1,14 +1,14 @@
 package com.example;
 
 import com.example.warehouse.DependencyFactory;
-import com.example.warehouse.TrialDependencyFactory;
+import com.example.warehouse.FullDependencyFactory;
 
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        DependencyFactory dependencyFactory = new TrialDependencyFactory();
+        DependencyFactory dependencyFactory = new FullDependencyFactory();
         new App(List.of(args), dependencyFactory).run();
     }
 }
